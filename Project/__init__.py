bbb4172d58ae3a67561b0c9477e7042427b23479
gtwn <56065981+gtwn@users.noreply.github.com>
2020-08-24 01:46:47 +0700

    connect to mongodb

--- Project/__init__.py
+++ Project/__init__.py
@@ -1,7 +1,21 @@
 from flask import Flask ,  request, abort
 import requests
 import json
+from datetime import datetime
 from Project.Config import *
+
+import pymongo
+from pymongo import MongoClient
+from Project.Config import *
+
+
+uri = "mongodb+srv://{}:{}@cluster0-aarl2.mongodb.net/{}?retryWrites=true&w=majority".format(DB_Username,DB_Password,DB_Name)
+cluster = pymongo.MongoClient(uri)
+
+db = cluster["{}".format(DB_Name)]
+
+collection = db["task"]
+
 
 app = Flask(__name__)
 
@@ -30,7 +44,9 @@ def GetUserProfile(userID,ChannelAccessToken):
         'Authorization': Authorization
     }
     resp = requests.get(api,headers=headers)
-    return resp.text
+    jsLoads = json.loads(resp.text)
+    print("jsLoads:",jsLoads)
+    return jsLoads["displayName"]
 
 def GetGroupUserProfile(userID,GroupID,ChannelAccessToken):
     api = 'https://api.line.me/v2/bot/group/{}/member/{}'.format(GroupID,userID)
@@ -40,7 +56,7 @@ def GetGroupUserProfile(userID,GroupID,ChannelAccessToken):
     }
     resp = requests.get(api,headers=headers)
     jsLoads = json.loads(resp.text)
-    return jsLoads['displayName']
+    return jsLoads["displayName"]
 
 @app.route('/')
 def hello():
@@ -56,6 +72,8 @@ def webhook():
         sourceType = payload['events'][0]['source']['type']
         if sourceType == 'group' :
             groupID = payload['events'][0]['source']['groupId']
+        else:
+            groupID = ' '
         if 'Hi' in message :
             userID = payload['events'][0]['source']['userId']
             replyMessage = 'Say Hi from ' + userID + 'in groupID' + groupID
@@ -68,17 +86,81 @@ def webhook():
             ReplyMessage(replyToken,replyMessage,Channel_Access_Token)
         if 'jeny' in message.lower() and '#order' in message.lower():
             userID = payload['events'][0]['source']['userId']
-            orderTo,task,by = SubString(message)
-            replyMessage = 'สั่งงานคุณ: {}\nรายละเอียดงาน: {}\nกำหนดส่ง: {}\nสั่งโดย: {}'.format(orderTo,task,by,GetGroupUserProfile(userID,groupID,Channel_Access_Token))
+            userProfile = GetUserProfile(userID,Channel_Access_Token)
+            orderTo,task,by = InsertTask(message,userProfile)
+            replyMessage = 'สั่งงานคุณ: {}\nรายละเอียดงาน: {}\nกำหนดส่ง: {}\nสั่งโดย: {}'.format(orderTo,task,by,userProfile)
             ReplyMessage(replyToken,replyMessage,Channel_Access_Token)
+        if 'push' in message :
+            PushMessage(Channel_Access_Token)
+        if 'งาน' in message or 'มีงาน' in message :
+            userID = payload['events'][0]['source']['userId']
+            profile = GetUserProfile(userID,Channel_Access_Token)
+            ReplyTaskMessage(replyToken,FindTask(profile),Channel_Access_Token)
+
         return request.json,200
+        
     elif request.method == 'GET' :
         return 'this is method GET', 200
 
     else:
         abort(400)
 
+def FindTask(userProfile):
+    reply = []
+    # results = collection.find({"order_to":userProfile},{"_id":0,"order_to":0,"task":1,"deadline":1,"created_at":1,"order_by":1,"done_at":0})
+    results = collection.find({"order_to":userProfile})
+    for result in results:
+        messageBack = {"type":"text",
+                    "text":"งาน : {}\nกำหนดส่ง : {}\nสั่งโดย : {}\nตั้งแต่วันที่ : {}".format(result["task"],result["deadline"],result["order_by"],result["created_at"])
+                    }
+        reply.append(messageBack)
+    return reply
 
+def InsertTask(message,userProfile):
+    nameTag = ''
+    tagCount = message.count('@')
+    task = message.lower().split("#task ")[1].split("#")[0]
+    if '#by' not in message.lower() :
+        by = 'To night'
+    else :
+        by = message.lower().split("#by ")[1].split()[0]
+    for i in range (tagCount) :
+        if i+1 == tagCount:
+            tag = message.split('@')[i+1].split(' ')[0]
+        else:
+            tag = message.split('@')[i+1]
+        nameTag += tag + ' '
+        data = {"order_to":tag,
+                "task":task,
+                "deadline":by,
+                "created_at":datetime.now(),
+                "done_at":"",
+                "order_by":userProfile}
+
+        collection.insert_one(data)
+
+    return nameTag,task,by
+
+def ReplyTaskMessage(ReplyToken,ReplyMessage,ChannelAccessToken):
+    LINE_API = 'https://api.line.me/v2/bot/message/reply'
+
+    Authorization = 'Bearer {}'.format(ChannelAccessToken)
+
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': Authorization
+    }
+
+    data = {
+        "replyToken": ReplyToken,
+        "messages":ReplyMessage
+    }
+    data = json.dumps(data) ## Dump dict >> Json obj
+    print('data : ',data)
+
+    r = requests.post(LINE_API,headers=headers,data=data)
+    
+    return 200
 
 def ReplyMessage(ReplyToken, TextMessage, ChannelAccessToken):
     LINE_API = 'https://api.line.me/v2/bot/message/reply'
@@ -94,6 +176,9 @@ def ReplyMessage(ReplyToken, TextMessage, ChannelAccessToken):
         "replyToken": ReplyToken,
         "messages":[{
             "type":"text",
+            "text":"รับทราบค่ะ"
+        },{
+            "type":"text",
             "text":TextMessage
         }]
     }
@@ -104,16 +189,25 @@ def ReplyMessage(ReplyToken, TextMessage, ChannelAccessToken):
     
     return 200
 
+def PushMessage(ChannelAccessToken):
+    api = 'https://api.line.me/v2/bot/message/multicast'
+    Authorization = 'Bearer {}'.format(ChannelAccessToken)
 
-def SubString(message):
-    nameTag = ''
-    for i in range (message.count('@')) :
-        tag = message.split('@')[i+1].split(' ')[0]
-        nameTag += tag + ' '
-    task = message.split(" ")[3]
-    if '#by' not in message.lower() :
-        by = 'To night'
-    else :
-        by = message.lower().split("#by ")[1]
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': Authorization
+    }
 
-    return nameTag,task,by
+    data = {
+        "to": ["Ucb533ae43957da0da8ef0289843f6ae6","U4bb6255362dbd86c9b941d36f85cb8d1","Uae4fc581117126f7ac87e1096ed77ead"],
+        "messages":[{
+            "type":"text",
+            "text":"มีข้อความเข้าในกลุ่ม CE-Project 2020 ค่ะ"
+        }]
+    }
+    data = json.dumps(data)
+
+    r = requests.post(api,headers=headers,data=data)
+    
+    return 200
+
