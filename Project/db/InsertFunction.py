0a628e9c84b48ea42cf3a8cc990514f16cade748
gtwn <56065981+gtwn@users.noreply.github.com>
2021-03-18 19:53:13 +0700

    fix insertTask tpye single

--- Project/db/InsertFunction.py
+++ Project/db/InsertFunction.py
@@ -14,21 +14,44 @@ tasklistDB = db["tasks"]
 def InsertNewTask(userList,member, subject, detail, typeWork, deadline, userOrderId, groupId):
     userProfile = GetUserProfile(userOrderId,Channel_Access_Token)
     groupName = GetGroupSummary(groupId, Channel_Access_Token)
-    data = {
-        "task":     subject.strip(),
-        "detail":   detail,
-        "deadline": deadline,
-        "created_at":datetime.now(),
-        "done_at":  datetime.min,
-        "order_by": userProfile,
-        "from_id":  userOrderId,
-        "group_id": groupId,
-        "group_name": groupName,
-        "type":     typeWork,
-        "member":   member,
-        "member_id": userList,
-        "status":   "In Progress"
-    }
-    tasklistDB.insert_one(data)
+    if typeWork == 'single':
+        for u in userList:
+            profile = GetUserProfile(u, Channel_Access_Token)
+            data = {
+                "task":     subject.strip(),
+                "detail":   detail,
+                "deadline": deadline,
+                "created_at":datetime.now(),
+                "done_at":  datetime.min,
+                "order_by": userProfile,
+                "from_id":  userOrderId,
+                "group_id": groupId,
+                "group_name": groupName,
+                "type":     typeWork,
+                "member":   [profile],
+                "member_id": [u],
+                "status":   "In Progress"
+            }
+            tasklistDB.insert_one(data)
+    else:
+
+        data = {
+            "task":     subject.strip(),
+            "detail":   detail,
+            "deadline": deadline,
+            "created_at":datetime.now(),
+            "done_at":  datetime.min,
+            "order_by": userProfile,
+            "from_id":  userOrderId,
+            "group_id": groupId,
+            "group_name": groupName,
+            "type":     typeWork,
+            "member":   member,
+            "member_id": userList,
+            "status":   "In Progress"
+        }
+        tasklistDB.insert_one(data)
 
     return userProfile
+
+
